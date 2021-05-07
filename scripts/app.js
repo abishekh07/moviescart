fd78e649ad9f44c97568bc23774237aa5503d6f3
Abishek H <habishek07@gmail.com>
2021-05-07 20:18:46 +0530

    pagination logic

--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,47 @@
 
 import { generateCard } from "./helpers.js"
 
-const api_url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&language=en-US&include_video=false&page=1`
+let page_api = 1
+let api_url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&language=en-US&include_video=false&page=${page_api}`
 
 const movieForm = document.querySelector(".movie__form")
 const moviesContainer = document.querySelector("main.movies")
 const loader = document.querySelector(".loader")
 
+function toggleLoader(shouldDisplay) {
+  if (shouldDisplay) {
+    loader.style.display = "flex"
+    document.body.style.overflow = "hidden"
+  } else {
+    setTimeout(() => {
+      loader.style.display = "none"
+      document.body.style.overflow = "unset"
+    }, 400)
+  }
+}
+
 function init(url) {
+  toggleLoader(1)
+
   fetch(url)
     .then((response) => response.json())
-    .then((movies) => displayMovies(movies.results))
+    .then((movies) => {
+      let validResponse = movies.total_results
+
+      if (!validResponse) {
+        init(api_url)
+      }
+
+      toggleLoader(0)
+      document.querySelector(".footer").style.visibility = "visible"
+      return displayMovies(movies.results)
+    })
     .catch((error) => {
       console.log(error)
     })
 }
 
-function setAPIUrl(newUrl) {
+function renderSearchResults(newUrl) {
   document.querySelector(".movies").innerHTML = ""
   init(newUrl)
 }
@@ -34,7 +59,7 @@ movieForm.addEventListener("submit", (e) => {
 
   let url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&page=1&include_adult=false&query=${searchQuery}`
 
-  setAPIUrl(url)
+  renderSearchResults(url)
 
   setTimeout(() => {
     e.target.movieInput.value = ""
@@ -68,22 +93,59 @@ function displayMovies(movieList) {
 
   const movies = moviesContainer.children
 
-  for (let i = 0; i < movies.length; i++) {
-    let movie_id = movies[i].id
+  for (let movie of movies) {
+    let movie_id = movie.id
 
-    movies[i].addEventListener("click", () => {
+    movie.addEventListener("click", () => {
       location.assign(`./movie.html#${movie_id}`)
     })
   }
 }
 
-if (loader) {
-  document.body.style.overflow = "hidden"
-}
+// Pagination
 
-window.addEventListener("load", () => {
-  loader.style.display = "none"
-  document.body.style.overflow = "unset"
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+const pageNum = document.querySelector(".pagination__number")
+let pageNumValue = pageNum.textContent
+
+prevBtn.addEventListener("click", () => {
+  getPageNumber(pageNumValue, "decrement")
 })
+
+nextBtn.addEventListener("click", () => {
+  getPageNumber(pageNumValue, "increment")
+})
+
+function getPageNumber(currPageNum, status) {
+  if (
+    (currPageNum <= 1 && status === "decrement") ||
+    (currPageNum === 100 && status === "increment")
+  ) {
+    return
+  }
+
+  console.log(currPageNum)
+
+  switch (status) {
+    case "increment":
+      currPageNum = Number(currPageNum) + 1
+      pageNumValue = currPageNum
+      break
+
+    case "decrement":
+      currPageNum = Number(currPageNum) - 1
+      pageNumValue = currPageNum
+      break
+  }
+
+  pageNum.textContent = pageNumValue
+  page_api = pageNumValue
+
+  api_url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&language=en-US&include_video=false&page=${page_api}`
+
+  moviesContainer.innerHTML = ""
+  init(api_url)
+}
 
 init(api_url) // Load Application
